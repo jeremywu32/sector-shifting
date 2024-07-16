bbbd39a59987d80e3d2ee80aec31facb529d91b1
jeremywu32 <131681512+jeremywu32@users.noreply.github.com>
2024-07-16 17:32:00 +0800

    meow

--- 行业轮动/Util.py
+++ 行业轮动/Util.py
@@ -1,6 +1,16 @@
 import pandas as pd
 import numpy as np
 
+'''
+use a dictionary to store the style factors of each sector
+each item under the key is a dataframe with date index
+backtesting frame goes like this:
+- at the first step, calculate the standardised factor exposure of each style factor under its sector
+- next, use utility functions to collect the statistics in the rolling window, visit each item in the dictionary
+  via the list
+- the output is expected to be a signal series
+'''
+
 class Util:
     def __init__(self):
         self._year = 242
