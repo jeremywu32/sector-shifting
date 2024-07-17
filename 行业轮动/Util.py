7150780f1dac9343700918bea4d6e98cb99f1edf
jeremywu32 <131681512+jeremywu32@users.noreply.github.com>
2024-07-17 10:47:40 +0800

    hahahahahaha

--- 行业轮动/Util.py
+++ 行业轮动/Util.py
@@ -32,10 +32,37 @@ class Util:
         diff = diff.dropna(how='any')
         return diff
 
+   '''
+    general assumption: data is at monthly frequency
+                        dataframes are datetime indexed
+    那个啥，你的返回类型可能会爆炸~~~~~~~~
+    '''
+
     def get_zscore(self, series: pd.DataFrame, years):
         # series shall apply datetime index
         median = self.median(series, years)
         std = self.std(series, years)
         sub_series = series.loc[series.index >= median.index[0], ]
         return (sub_series - median) / std
+    
+    def get_sue_3yr(self, series: pd.DataFrame):
+        past_3yr_series = series.shift(36)
+        past_3yr_series = past_3yr_series.dropna(how='any')
+        benchmark_time = past_3yr_series.index[0]
+        past_2yr_series = series.shift(24)
+        past_2yr_series = past_2yr_series.dropna(how='any')
+        past_2yr_series = past_2yr_series.loc[past_2yr_series.index >= benchmark_time, ]
+        past_1yr_series = series.shift(12)
+        past_1yr_series = past_1yr_series.dropna(how='any')
+        past_1yr_series = past_1yr_series.loc[past_1yr_series.index >= benchmark_time, ]
+        current_series = series.loc[series.index >= benchmark_time, ]
+        std = self.std(series, 3)
+        past_ave_series = np.divide(past_1yr_series.iloc[:, 0] + past_2yr_series.iloc[:, 0] + past_3yr_series.iloc[:, 0], 3)
+        past_ave_series.index = past_3yr_series.index
+        return (current_series - past_ave_series) / std
+        
+    def marginal_change(self, series: pd.DataFrame):
+        three_mo_ago = series.shift(3)
+        three_mo_ago = three_mo_ago.dropna(how='any')
+        return series - three_mo_ago
 
