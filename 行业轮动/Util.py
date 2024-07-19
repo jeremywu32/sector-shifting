f7a51e799ff64868381cb22c2de746c7c52b3336
jeremywu32 <131681512+jeremywu32@users.noreply.github.com>
2024-07-19 19:02:24 +0800

    Update Util.py

--- 行业轮动/Util.py
+++ 行业轮动/Util.py
@@ -1,16 +1,6 @@
 import pandas as pd
 import numpy as np
 
-'''
-use a dictionary to store the style factors of each sector
-each item under the key is a dataframe with date index
-backtesting frame goes like this:
-- at the first step, calculate the standardised factor exposure of each style factor under its sector
-- next, use utility functions to collect the statistics in the rolling window, visit each item in the dictionary
-  via the list
-- the output is expected to be a signal series
-'''
-
 class Util:
     def __init__(self):
         self._year = 242
@@ -32,19 +22,25 @@ class Util:
         diff = diff.dropna(how='any')
         return diff
 
-   '''
+    '''
     general assumption: data is at monthly frequency
                         dataframes are datetime indexed
     那个啥，你的返回类型可能会爆炸~~~~~~~~
     '''
 
-    def get_zscore(self, series: pd.DataFrame, years):
+    def get_zcsore_static(self, series: pd.DataFrame):
+        mean = np.mean(series)
+        std = np.std(series)
+        diff = series - [mean for _ in range(series.shape[0])]
+        return np.divide(diff, std)
+
+    def get_zscore_rolling(self, series: pd.DataFrame, years):
         # series shall apply datetime index
         median = self.median(series, years)
         std = self.std(series, years)
         sub_series = series.loc[series.index >= median.index[0], ]
         return (sub_series - median) / std
-    
+
     def get_sue_3yr(self, series: pd.DataFrame):
         past_3yr_series = series.shift(36)
         past_3yr_series = past_3yr_series.dropna(how='any')
@@ -60,9 +56,8 @@ class Util:
         past_ave_series = np.divide(past_1yr_series.iloc[:, 0] + past_2yr_series.iloc[:, 0] + past_3yr_series.iloc[:, 0], 3)
         past_ave_series.index = past_3yr_series.index
         return (current_series - past_ave_series) / std
-        
+
     def marginal_change(self, series: pd.DataFrame):
         three_mo_ago = series.shift(3)
         three_mo_ago = three_mo_ago.dropna(how='any')
         return series - three_mo_ago
-
