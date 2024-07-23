ea3d80de680f7b22025a823f167452c3b1c27c05
jeremywu32 <131681512+jeremywu32@users.noreply.github.com>
2024-07-23 09:19:05 +0800

    Update DataLoading.py

--- 行业轮动/DataLoading.py
+++ 行业轮动/DataLoading.py
@@ -16,7 +16,7 @@ backtesting frame goes like this:
 
 class DataLoading:
     def __init__(self, macro_data: pd.DataFrame, bond_data: pd.DataFrame, market_data: pd.DataFrame, init_date):
-        self._macro_data = macro_data.loc[market_data.index >= init_date, ]
+        self._macro_data = macro_data.loc[macro_data.index >= init_date, ]
         self._bond_data = bond_data.loc[bond_data.index >= init_date, ]
         self._market_data = market_data.loc[market_data.index >= init_date, ]
         self._macro_dict = {}
@@ -44,49 +44,34 @@ class DataLoading:
         # group data into class A and class B
         # handle time lagging problem
         self._macro_dict['pmi'] = self._macro_data.iloc[:, 2]
-        self._macro_dict['pmi'].index = self._market_data.index
-        self._macro_dict['pmi_ma12'] = util.ma(self._macro_data.iloc[:, 2], 242)
-        self._macro_dict['pmi_ma12'].index = self._market_data.index
+        self._macro_dict['pmi_ma12'] = util.ma(self._macro_data.iloc[:, 2], 12)
         self._macro_dict['ppi'] = (self._macro_data.iloc[:, 3]).shift(1)
-        self._macro_dict['ppi'].index = self._macro_data.index
         self._macro_dict['cpi'] = (self._macro_data.iloc[:, 1]).shift(1)
-        self._macro_dict['cpi'].index = self._macro_data.index
         self._macro_dict['m0'] = (self._macro_data.iloc[:, 5]).shift(1)
-        self._macro_dict['m0'].index = self._macro_data.index
         self._macro_dict['m1'] = (self._macro_data.iloc[:, 6]).shift(1)
-        self._macro_dict['m1'].index = self._macro_data.index
         self._macro_dict['m2'] = (self._macro_data.iloc[:, 7]).shift(1)
-        self._macro_dict['m2'].index = self._macro_data.index
         self._macro_dict['m1_m0'] = (self._macro_data.iloc[:, 6] - self._macro_data.iloc[:, 5]).shift(1)
-        self._macro_dict['m1_m0'].index = self._macro_data.index
         self._macro_dict['rate_10yr'] = self._bond_data.iloc[:, 2]
-        self._macro_dict['rate_10yr'].index = self._macro_data.index
         self._macro_dict['rate_gap_10yr'] = self._bond_data.iloc[:, 2] - self._bond_data.iloc[:, 1]
-        self._macro_dict['rate_gap_10yr'].index = self._macro_data.index
         self._macro_dict['financing'] = (self._macro_data.iloc[:, 7]).shift(1)
-        self._macro_dict['financing'].index = self._macro_data.index
         self._macro_dict['financing_delta'] = (self._macro_data.iloc[:, 8]).shift(1)
-        self._macro_dict['financing_delta'].index = self._macro_data.index
         self._macro_dict['a_share_index_erp'] = self._set_a_share_index_erp()
-        self._macro_dict['a_share_index_erp'].index = self._macro_data.index
         self._macro_dict['sp500_erp'] = self._set_sp500_erp()
-        self._macro_dict['sp500_erp'].index = self._macro_data.index
         self._macro_dict['sp500_vix'] = self._set_sp500_vix()
-        self._macro_dict['sp500_vix'].index = self._macro_data.index
 
     def get_macro_dict(self):
+        self._arrange_data()
         return self._macro_dict
 
     def _set_a_share_index_erp(self):
-        a_share_ep = np.divide(1, self._market_data.iloc[:, ])
+        a_share_ep = np.divide(100, self._market_data.iloc[:, 1])
         erp = a_share_ep - self._macro_dict["rate_10yr"]
         return erp
 
-
     def _set_sp500_erp(self):
-        sp_500_erp = np.divide(1, self._market_data.iloc[:, ])
+        sp_500_erp = np.divide(100, self._market_data.iloc[:, 4])
         erp = sp_500_erp - self._macro_dict["rate_10yr"]
         return erp
 
     def _set_sp500_vix(self):
-        return self._market_data.iloc[:, ]
+        return self._market_data.iloc[:, 3]
